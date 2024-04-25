99ecacc66775fdf9ac8e511e8649ea509ab70bec
sander-struijk <sander.struijk@webstep.no>
2024-04-25 15:47:50 +0200

    github actions - failure

--- devops/hello/src/test.js
+++ devops/hello/src/test.js
@@ -4,5 +4,5 @@ import assert from 'node:assert/strict';
 console.log("Running tests...");
 
 // a failed assert will print error and make the process return a non-zero exit code.
-assert(snakeCase("Hello, world!") == "hello_world");
+assert(snakeCase("Hello, world!") == "HelloWorld");
 
